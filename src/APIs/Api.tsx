8a1f5e753a7f83952af907360a12635a0e04ed12
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-11-09 03:41:04 -0400

    unadded obs

---
+++ src/APIs/Api.tsx
@@ -0,0 +1,35 @@
+type Event = {
+  event_name: string;
+  event_description: string;
+  event_details: string;
+  event_venue: string;
+  event_duration: string;
+};
+
+export const GetEvents = async (): Promise<Event[]> => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_FETCH_ROUTE}/events`);
+    const returnedData = response.json();
+
+    return returnedData;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+export const GetSearchedEvents = async (
+  searchParam: string
+): Promise<Event[]> => {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_FETCH_ROUTE}/events/search/${searchParam}`
+    );
+    const returnedData = response.json();
+
+    return returnedData;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
